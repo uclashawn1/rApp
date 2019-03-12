d7f58431a812bc4c3cdedb65e449487c9d97dcfe
Fooji <jfujitsubo@yahoo.com>
2019-03-11 20:36:30 -0700

    server

--- routes/tables.js
+++ routes/tables.js
@@ -6,9 +6,18 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 4000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 
---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 4000;
+
+// Sets up the Express app to handle data parsing
+/*app.use(express.urlencoded({ extended: true }));
+app.use(express.json());*/
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
