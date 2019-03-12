f14345add7fffd178f05731c05e0111d965347f1
UCLAShawn3G <uclashawn1@gmail.com>
2019-03-11 21:27:21 -0700

    app post clear table array data

--- routes/clear.js
+++ routes/clear.js
@@ -1,28 +1,17 @@
 
 
-    
-    // This function resets all of the data in our tables. This is intended to let you restart a demo.
-	function clearTable(){
-
-		var currentURL = window.location.origin;
-		$.ajax({url: currentURL + "/api/clear", method: "POST"})
-
+	function clearTable () {
+			
+		app.post('/api/clear', function()   {
+			tableData = [];
+			waitListData = [];
+	
+			console.log(tableData);
+			console.log(waitListData);
+		})
 	}
 
-	$("#clear").on('click', function(){
-		alert("Clearing...");
-		clearTable();
-
-		// Refresh the page after data is cleared
-		location.reload();
-	})
-
-
-	// Run Queries!
-	// ==========================================
-	runTableQuery();
-	runWaitListQuery();
-
+	
 	// make modules available for server.js
 	module.exports.clearTable;
 
